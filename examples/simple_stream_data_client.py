6c62079f8da3f9e6928cd83f56638a53758ea32d
Kamil Kurach <kamilkurach@protonmail.com>
2022-10-22 02:42:53 +0200

    Add server and client

--- examples/simple_stream_data_client.py
+++ examples/simple_stream_data_client.py
@@ -1,1 +1,33 @@
 """Reads data stream from RPi (IMU, camera)"""
+import socket            
+
+
+class SimpleStreamClient:
+    def __init__(self):
+        # self.driver = self._create_spi_driver()
+        # self.driver = self._create_i2c_driver()
+        # self.driver.configure()
+        self.data = {}
+        self.s = socket.socket()
+        self.port = 8888
+        
+ 
+    def connect(self):
+        self.s.connect(('127.0.0.1', self.port))
+
+    def read_data(self):
+        print(self.s.recv(1024).decode())
+
+    def close_connection(self):
+        s.close()   
+    
+    def main(self):
+        self.connect()
+
+        while True:
+            self.read_data()
+              
+        self.close_connection()
+
+if __name__ == '__main__':
+    SimpleStreamClient().main()
--- examples/simple_stream_data_server.py
+++ examples/simple_stream_data_server.py
@@ -1,64 +1,84 @@
-# from lsm9ds1_rjg import Driver, I2CTransport, SPITransport
 import sys
 import os
 import time
 import datetime as dt
 import random as r
+import socket
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
+from lsm9ds1_rjg import Driver, I2CTransport, SPITransport
 
-
-class SimpleStream:
+class SimpleStreamServer:
     """Streams sensor data from RPi over localhost"""
     """This example shows how to poll the sensor for new data.
     It queries the sensor to discover when the accelerometer/gyro
     has new data and then reads all the sensors."""
 
     def __init__(self):
-        # self.driver = self._create_spi_driver()
-        # self.driver = self._create_i2c_driver()
-        # self.driver.configure()
+        self.driver = self._create_spi_driver()
+        self.driver = self._create_i2c_driver()
+        self.driver.configure()
         self.data = {}
+        self.s = socket.socket()
+        self.port = 8888
+        self.init_socket()
 
-    # @staticmethod
-    # def _create_i2c_driver() -> Driver:
-    #     return Driver(
-    #         I2CTransport(1, I2CTransport.I2C_AG_ADDRESS),
-    #         I2CTransport(1, I2CTransport.I2C_MAG_ADDRESS))
+    def init_socket(self):
+        try:
+            self.s.bind(('',int(self.port)))
+        except Exception as Error:
+            print(Error)
 
-    # @staticmethod
-    # def _create_spi_driver() -> Driver:
-    #     return Driver(
-    #         SPITransport(0, False),
-    #         SPITransport(1, True))
+        self.s.listen(5)
+        print("init socket")
+
+    @staticmethod
+    def _create_i2c_driver() -> Driver:
+        return Driver(
+            I2CTransport(1, I2CTransport.I2C_AG_ADDRESS),
+            I2CTransport(1, I2CTransport.I2C_MAG_ADDRESS))
+
+    @staticmethod
+    def _create_spi_driver() -> Driver:
+        return Driver(
+            SPITransport(0, False),
+            SPITransport(1, True))
 
     def main(self):
         try:
             count = 0
-            # while True:
-            while count < 500:
-                # ag_data_ready = self.driver.read_ag_status().accelerometer_data_available
-                # if ag_data_ready:
-                if True:
-                    self.generate_dummy_data()
-                    self.print_data()
-                    time.sleep(0.5)
-                    # self.stream_data()
+            c, addr = self.s.accept()
+            print("connection from {0}".format(addr))
+            while True:
+            # while count < 100:
+                ag_data_ready = self.driver.read_ag_status().accelerometer_data_available
+                if ag_data_ready:
+                    try:
+                        # self.generate_dummy_data()
+                        # self.print_data()
+                        self.read_data()
+                        self.stream_data(c)
+                    except:
+                        self.init_socket()
+                        c.send(str(self.data).encode())
                     count += 1
                 else:
                     time.sleep(0.00001)
         finally:
-            # self.driver.close()
-            pass
+            self.driver.close()
+            c.close()
 
-    # def read_data(self):
-    #     temp, acc, gyro = self.driver.read_ag_data()
-    #     mag = self.driver.read_magnetometer()
-    #     time = datetime.datetime.now()
-    #     self.data = {'temp': temp, 'acc_x': acc[0], 'acc_y': acc[1], 'acc_z': acc[2],
-    #                  'gyro_x': gyro[0], 'gyro_y': gyro[1], 'gyro_z': gyro[2],
-    #                  'mag_x': mag[0], 'mag_y': mag[1], 'mag_z': mag[2],
-    #                                   'time': time}
+    def stream_data(self, c):
+        c.send(str(self.data).encode())
+
+    def read_data(self):
+        temp, acc, gyro = self.driver.read_ag_data()
+        mag = self.driver.read_magnetometer()
+        time = datetime.datetime.now()
+        self.data = {'temp': temp, 'acc_x': acc[0], 'acc_y': acc[1], 'acc_z': acc[2],
+                     'gyro_x': gyro[0], 'gyro_y': gyro[1], 'gyro_z': gyro[2],
+                     'mag_x': mag[0], 'mag_y': mag[1], 'mag_z': mag[2],
+                                      'time': time}
 
     def generate_dummy_data(self):
         temp = [r.randint(80, 110)]
@@ -70,10 +90,9 @@ class SimpleStream:
                      'gyro_x': gyro[0], 'gyro_y': gyro[1], 'gyro_z': gyro[2],
                      'mag_x': mag[0], 'mag_y': mag[1], 'mag_z': mag[2],
                                       'time': d_t}
-        
+
     def print_data(self):
         print(self.data)
 
-
 if __name__ == '__main__':
-    SimpleStream().main()
+    SimpleStreamServer().main()
